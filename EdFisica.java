2d3742ea6ad8c2c6a4d4b0b02fe0eba444dfd90d
uesleisutil <ueslei@outlook.com>
2020-10-25 20:05:54 -0300

    Update

---
+++ EdFisica.java
@@ -0,0 +1,63 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class EdFisica {
+    public static void main(String[] args) {
+        List <String> nome_estudante  = new ArrayList<>();
+        List <Float> idade_estudante  = new ArrayList<>();
+        List <Float> altura_estudante = new ArrayList<>();
+        List <Float> peso_estudante   = new ArrayList<>();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Alunos na turma?");
+        int qtd_alunos = input.nextInt();
+    
+        Scanner input_notas = new Scanner(System.in);
+        for (int i = 1; i <= qtd_alunos; i++){
+            
+            float prova = 0;
+            float projeto = 0;
+            float lista = 0;
+        
+            System.out.println("Nota prova aluno "+ i + "?");  
+            prova = Float.parseFloat(input_notas.nextLine());
+            System.out.println("Nota projeto aluno "+ i + "?");  
+            projeto = Float.parseFloat(input_notas.nextLine());
+            System.out.println("Nota lista de exercícios aluno "+ i + "?");  
+            lista = Float.parseFloat(input_notas.nextLine());
+            
+            int   peso_prova = 5, peso_projeto = 3, peso_lista = 2;
+            float media_aluno;
+            media_aluno = (prova * peso_prova + projeto * peso_projeto + lista * peso_lista) / (peso_projeto + peso_prova + peso_lista);
+            medias_dos_alunos.add(media_aluno);
+            
+            if (media_aluno < 5) {
+                resultado_alunos.add("Reprovado");
+                alunos_reprovados =+ 1;
+            }
+            else if (media_aluno <= 7.9) {
+                resultado_alunos.add("Avaliação Final");
+                alunos_af =+ 1;
+            }
+            else {
+                resultado_alunos.add("Aprovado");
+                alunos_aprovados =+ 1;
+            }   
+        }
+        input.close();
+        input_notas.close();
+        System.out.println("Maior nota: "+ Collections.max(medias_dos_alunos));
+        System.out.println("Menor nota: "+ Collections.min(medias_dos_alunos));
+        float media_turma;
+        float soma_notas = 0;
+        
+        for (float nota : medias_dos_alunos) {
+            soma_notas =+ nota;
+        }
+        media_turma = soma_notas/qtd_alunos;
+        System.out.println("Média da turma: "+ media_turma);
+        System.out.println("Alunos aprovados: "+ alunos_aprovados);
+        System.out.println("Alunos com avaliação final: "+ alunos_af);
+        System.out.println("Alunos reprovados: "+ alunos_reprovados);
+            }
+    }
