9e24aa74d6caaa053585adc449e32b69abc65171
uesleisutil <ueslei@outlook.com>
2020-10-25 22:47:33 -0300

    Update

--- LuDas.js
+++ LuDas.js
@@ -1,17 +1,29 @@
-var nome   = prompt("Nome do aluno:");
-var altura = prompt("Altura do aluno:");
-var peso   = prompt("Peso do aluno:");
-var imc    = peso/(altura*altura);
+function calculaDesconto(valor){
+    let valor_venda;
+    let desconto1 = 0.1, desconto2 = 0.15, desconto_aplicado;
 
-if(imc <= 18.5){
-    document.write("Resultado: Seu IMC é:"+imc+". Você está abaixo do peso. Classificação: Magreza Grau 0");
-    }else if(imc >18.5 && imc< 24.9){
-    document.write(" Resultado: Seu IMC é:"+imc+". Seu peso está normal Classificação: Normal Grau 0");
-}else if(imc >25 && imc < 29.9){
-    alert("Resultado: Seu IMC é:"+imc+". Você está acima do peso. Classificação: Sobrepeso Grau 1 ");
-}else if (imc>30 && imc< 39.9) {
-    alert(" Resultado: Seu IMC é:"+imc+". Você está acima do peso \nClassificação: Obesidade\nGrau 2 ");
-}else if(imc >40.0) {
-    alert("Resultado: Seu IMC é de:"+imc+". Você está acima do peso \nClassificação: Obesidade Grave\nGrau 3 ");
+    if (parseFloat(valor) < 0){
+        return "Valor inválido. A venda deve ser positiva"
+    }
+    else if (parseFloat(valor) <= 499.00){
+        valor_venda = valor * (1 - desconto1);
+        desconto_aplicado = desconto1;
+    }
+    else{
+        valor_venda = valor * (1 - desconto2);
+        desconto_aplicado = desconto2;
+    }
 
+    return {
+        "desconto": desconto_aplicado*100+"%",
+        "valor_venda": valor_venda,
+        "valor_desconto": valor-valor_venda
+        }
 }
+
+
+console.log(calculaDesconto(-5))
+
+console.log(calculaDesconto(300))
+
+console.log(calculaDesconto(500))
